751e9cd9629a5c2b28452439061c3e2bf2a392ad
naterattner <naterattner@gmail.com>
2022-10-09 21:34:33 -0400

    horizontal bar chart

--- 2_1_quantities_and_amounts/main.js
+++ 2_1_quantities_and_amounts/main.js
@@ -1,18 +1,69 @@
+/* reference: https://bl.ocks.org/hrecht/f84012ee860cb4da66331f18d588eee3 */
 
 /* CONSTANTS AND GLOBALS */
-// const width = ;
-// const height = ;
+//we need some room on the left for labels
+var margin = {
+  top: 15,
+  right: 60,
+  bottom: 15,
+  left: 0
+};
+
+const width = 500 - margin.left-margin.right;
+const height = window.innerHeight *.8 ;
+
+
 
 /* LOAD DATA */
-d3.csv('../[PATH_TO_YOUR_DATA]', d3.autoType)
-  .then(data => {
-    console.log("data", data)
+d3.csv('../data/squirrelActivities.csv', d3.autoType)
+.then(data => {
+  console.log("data", data)
+  
+  // yscale - categorical, activity
+  const yScale = d3.scaleBand()
+    .domain(data.map(d=> d.activity))
+    .range([0, height])
+    .paddingInner(.2)
 
-    /* SCALES */
-    /** This is where you should define your scales from data to pixel space */
-    
+  //xscale - linear,count   
+  const xScale = d3.scaleLinear()
+    .domain([0, d3.max(data, d=> d.count)])
+    .range([width, 0]) 
 
-    /* HTML ELEMENTS */
-    /** Select your container and append the visual elements to it */
+  /* HTML ELEMENTS */
+  // svg
+  const svg = d3.select("#container")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-  })
+  // bars
+  svg.selectAll("rect")
+    .data(data)
+    .join("rect")
+    .attr("height", yScale.bandwidth())
+    .attr("width", d=> width - xScale(d.count))
+    .attr("y", d=>yScale(d.activity))
+    .attr("x", d=> xScale(d.count))
+    // .attr("x", 0)
+
+    //y axis and show bar names
+    // const yAxis = d3.axisLeft(yScale)
+    // console.log(yAxis)
+    // .scale(yScale)
+    // //no tick marks
+    // .tickSize(0)
+    // .orient("left");
+
+    //y axis and show bar names
+    svg.append("g")
+      .call(d3.axisRight(yScale))
+      .attr("transform", `translate(${width + margin.right-50},0)`);
+
+    svg.append("g")
+    .call(d3.axisBottom(xScale))
+    .attr("transform", `translate(0,${height + (margin.bottom-2)})`);
+      
+
+})
